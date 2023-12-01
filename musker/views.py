b1ee9273d9dfd225f0707ed5cca65959684fd70b
Nuwangack <147961836+Nuwangack@users.noreply.github.com>
2023-12-02 01:34:35 +0530

    Update views.py

--- musker/views.py
+++ musker/views.py
@@ -234,7 +234,15 @@ def edit_cipher(request, pk):
 def search(request):
     if request.method == "POST":
 	search = request.POST['search']
-	searched = cipher.objects.filter(body__contains = search)
+	searched = cipher.objects.filter(username__contains = search)
         return render(request, 'search.html', {'search':search}, 'searched':searched)
     else:
 	return render(request, 'search.html', {})
+
+def search_user(request):
+    if request.method == "POST":
+	search = request.POST['search']
+	searched = User.objects.filter(body__contains = search)
+        return render(request, 'search_user.html', {'search':search}, 'searched':searched)
+    else:
+	return render(request, 'search_user.html', {})
